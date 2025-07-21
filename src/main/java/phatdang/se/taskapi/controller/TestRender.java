e8ddce5dc1e6c359ea73f4508ffff35392fbd4bf
phatdang <phatdang19052004@gmail.com>
2025-07-21 18:47:49 +0700

    Kiểm thử triển khai render

---
+++ src/main/java/phatdang/se/taskapi/controller/TestRender.java
@@ -0,0 +1,13 @@
+package phatdang.se.taskapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRender {
+    @GetMapping("sayHello")
+    public String sayHello(){
+        return "Hello render";
+    }
+}
