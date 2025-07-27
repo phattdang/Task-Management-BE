a0e2fbc714e93c5e878b88fd808e38ab28632263
phattdang <phatdang19052004@gmail.com>
2025-07-27 18:31:58 +0700

    refactor(converter): thêm code vớ vẩn
    Chưa đọc code

--- src/main/java/phatdang/se/taskapi/converter/JpaConverterJson.java
+++ src/main/java/phatdang/se/taskapi/converter/JpaConverterJson.java
@@ -1,4 +1,28 @@
 package phatdang.se.taskapi.converter;
 
-public class JpaConverterJson {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.*;
+
+@Converter(autoApply = false)
+public class JpaConverterJson implements AttributeConverter<String, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Convert to JSON failed", e);
+        }
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, String.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Convert from JSON failed", e);
+        }
+    }
 }
