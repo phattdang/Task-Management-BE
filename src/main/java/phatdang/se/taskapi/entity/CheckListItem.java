31502fd92dbaf745a3ccd699191c268feadf7ca2
phattdang <phatdang19052004@gmail.com>
2025-07-27 16:06:51 +0700

    feat(entity): thêm entity CheckListItem
    Hiện tại chưa biết để làm gì

---
+++ src/main/java/phatdang/se/taskapi/entity/CheckListItem.java
@@ -0,0 +1,13 @@
+package phatdang.se.taskapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CheckListItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
