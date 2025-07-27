234785fd0186760a2763365cf5b9a0e17407f60d
phattdang <phatdang19052004@gmail.com>
2025-07-27 18:35:49 +0700

    feat(dto.request): thêm CreationTaskRequest
    Thêm dto tạo task

---
+++ src/main/java/phatdang/se/taskapi/dto/request/CreationTaskRequest.java
@@ -0,0 +1,27 @@
+package phatdang.se.taskapi.dto.request;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import phatdang.se.taskapi.entity.Project;
+import phatdang.se.taskapi.entity.User;
+import phatdang.se.taskapi.enums.TaskPriority;
+import phatdang.se.taskapi.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class CreationTaskRequest {
+    String taskName;
+    String createdAt;
+    String userId;
+    Long projectId;
+}
