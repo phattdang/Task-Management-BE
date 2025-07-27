ae97d9bf77e841de467d4423116716e9d0bd9819
phattdang <phatdang19052004@gmail.com>
2025-07-27 18:39:51 +0700

    feat(converter): thêm class TaskService
    Thêm class TaskService

---
+++ src/main/java/phatdang/se/taskapi/service/impl/TaskServiceImpl.java
@@ -0,0 +1,52 @@
+package phatdang.se.taskapi.service.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import phatdang.se.taskapi.converter.TaskConverter;
+import phatdang.se.taskapi.dto.request.CreationTaskRequest;
+import phatdang.se.taskapi.dto.response.TaskResponse;
+import phatdang.se.taskapi.entity.Task;
+import phatdang.se.taskapi.repository.ProjectRepository;
+import phatdang.se.taskapi.repository.TaskRepository;
+import phatdang.se.taskapi.repository.UserRepository;
+import phatdang.se.taskapi.service.TaskService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TaskServiceImpl implements TaskService {
+    TaskRepository taskRepository;
+    ProjectRepository projectRepository;
+    UserRepository userRepository;
+
+    @Override
+    public Task saveTask(CreationTaskRequest request) {
+        Task task = new Task();
+        task.setTaskName(request.getTaskName());
+
+        LocalDate date = LocalDate.parse(request.getCreatedAt(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDateTime createdAt = date.atStartOfDay(); // chuyển sang 00:00:00
+
+        task.setCreatedAt(createdAt);
+        task.setProject(projectRepository.findById(request.getProjectId()).orElse(null));
+        task.setAssignee(userRepository.findById(request.getUserId()).orElse(null));
+
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public List<TaskResponse> getTasks() {
+        return taskRepository.findAll()
+                .stream()
+                .map(TaskConverter::toTaskResponse)
+                .collect(Collectors.toList());
+    }
+}
